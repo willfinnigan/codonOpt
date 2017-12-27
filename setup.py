6b18df5e2bf0c84b05fdefa7c129c0ed519bcb92
William Finnigan <wjafinnigan@gmail.com>
2017-12-27 14:20:07 +0000

    0.2 Setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='codonOpt',
-    version='0.1',
+    version='0.2',
     packages=['codonOpt'],
     url='https://github.com/willfinnigan/codonOpt',
     license='MIT',
     author='William Finnigan',
     author_email='wjafinnigan@gmail.com',
     description='A package for codon optimisation',
-    download_url = 'https://github.com/willfinnigan/codonOpt/archive/0.1.tar.gz', # I'll explain this in a second
-    keywords = ['DNA', 'Codon Optimisation', 'Gene Design'] # arbitrary keywords
+    download_url = 'https://github.com/willfinnigan/codonOpt/archive/0.2.tar.gz',
+    keywords = ['DNA', 'Codon Optimisation', 'Gene Design']
 )
