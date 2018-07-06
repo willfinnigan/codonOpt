77f662dc5e30d1adac626c5a849a977af7f04940
William Finnigan <wjafinnigan@gmail.com>
2018-07-06 09:36:51 +0100

    Split off eval functions into SeqEval

--- codonOpt/__init__.py
+++ codonOpt/__init__.py
@@ -1,6 +1,8 @@
 __author__ = 'Will'
 
-from codonOpt.SeqMake import CodonTable, Sequence_Generator, DNA, Protein, make_dna, evaluate_motifs, evaluate_gc_content, evaluate_mfe
+from codonOpt.SeqMake import CodonTable, Sequence_Generator, DNA, Protein, make_dna
+from codonOpt.SeqEval import evaluate_motifs, evaluate_gc_content, evaluate_mfe
+
 
 
 
